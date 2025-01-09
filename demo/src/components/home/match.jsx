931f9baab9e55927fe262315ca3d163f0207ebff
quoc127 <nquoc160200@gmail.com>
2025-01-09 16:54:34 +0700

    match

---
+++ demo/src/components/home/match.jsx
@@ -0,0 +1,545 @@
+import Bed from "../../assets/bed.png";
+const data = {
+  code: 3,
+  data: {
+    events: [
+      {
+        id: "4m9applq5v8yr6b",
+        homeScore: {
+          current: 0,
+          display: 0,
+          period1: 0,
+          period2: 0,
+          corner: 4,
+          yellow_card: 2,
+          red_card: 0,
+          overTime_score: 0,
+          penalty_score: 0,
+        },
+        tournament: {
+          id: "ym2xwfiotyu669p",
+          priority: 9,
+          name: "La Liga",
+          slug: "la-liga",
+          category: {
+            name: "Spain",
+            slug: "spain",
+            id: "3hc6ro1wp0p3bmq",
+            flag: "spain",
+          },
+          group_num: 0,
+        },
+        homeTeam: {
+          id: "s4luxy1o0nbrihp",
+          name: "UD Las Palmas",
+          slug: "ud-las-palmas",
+          shortName: "Las Palmas",
+        },
+        awayScore: {
+          current: 2,
+          display: 2,
+          period1: 1,
+          period2: 1,
+          corner: 2,
+          yellow_card: 3,
+          red_card: 0,
+          overTime_score: 0,
+          penalty_score: 0,
+        },
+        awayTeam: {
+          id: "s4luxy1o353rihp",
+          name: "Girona FC",
+          slug: "girona-fc",
+          shortName: "Girona",
+        },
+        status: {
+          code: 100,
+          description: "ended",
+          type: "finished",
+        },
+        winnerCode: 2,
+        startTimestamp: 1714219200,
+        stage_id: "mx7a61lemw6se70",
+      },
+      {
+        id: "54cvbmlw373hvp4",
+        homeScore: {
+          current: 2,
+          display: 2,
+          period1: 1,
+          period2: 1,
+          corner: 4,
+          yellow_card: 2,
+          red_card: 0,
+          overTime_score: 0,
+          penalty_score: 0,
+        },
+        tournament: {
+          id: "ym2xwfiotyu669p",
+          priority: 9,
+          name: "La Liga",
+          slug: "la-liga",
+          category: {
+            name: "Spain",
+            slug: "spain",
+            id: "3hc6ro1wp0p3bmq",
+            flag: "spain",
+          },
+          group_num: 0,
+        },
+        homeTeam: {
+          id: "s4luxy1o0nbrihp",
+          name: "UD Las Palmas",
+          slug: "ud-las-palmas",
+          shortName: "Las Palmas",
+        },
+        awayScore: {
+          current: 2,
+          display: 2,
+          period1: 1,
+          period2: 1,
+          corner: 8,
+          yellow_card: 1,
+          red_card: 0,
+          overTime_score: 0,
+          penalty_score: 0,
+        },
+        awayTeam: {
+          id: "g38gsxooh9qrl7t",
+          name: "Real Betis",
+          slug: "real-betis",
+          shortName: "Real Betis",
+        },
+        status: {
+          code: 100,
+          description: "ended",
+          type: "finished",
+        },
+        winnerCode: 3,
+        startTimestamp: 1715880600,
+        stage_id: "mx7a61lemw6se70",
+      },
+      {
+        id: "o6jamrls9g17w7q",
+        homeScore: {
+          current: 0,
+          display: 0,
+          period1: 0,
+          period2: 0,
+          corner: 5,
+          yellow_card: 3,
+          red_card: 1,
+          overTime_score: 0,
+          penalty_score: 0,
+        },
+        tournament: {
+          id: "ym2xwfiotyu669p",
+          priority: 9,
+          name: "La Liga",
+          slug: "la-liga",
+          category: {
+            name: "Spain",
+            slug: "spain",
+            id: "3hc6ro1wp0p3bmq",
+            flag: "spain",
+          },
+          group_num: 0,
+        },
+        homeTeam: {
+          id: "ym2xwfiohpss006",
+          name: "Cadiz",
+          slug: "cadiz",
+          shortName: "Cadiz CF",
+        },
+        awayScore: {
+          current: 0,
+          display: 0,
+          period1: 0,
+          period2: 0,
+          corner: 3,
+          yellow_card: 1,
+          red_card: 0,
+          overTime_score: 0,
+          penalty_score: 0,
+        },
+        awayTeam: {
+          id: "s4luxy1o0nbrihp",
+          name: "UD Las Palmas",
+          slug: "ud-las-palmas",
+          shortName: "Las Palmas",
+        },
+        status: {
+          code: 100,
+          description: "ended",
+          type: "finished",
+        },
+        winnerCode: 3,
+        startTimestamp: 1716138000,
+        stage_id: "mx7a61lemw6se70",
+      },
+      {
+        id: "a8qv3rl0446xr1c",
+        homeScore: {
+          current: 1,
+          display: 1,
+          period1: 0,
+          period2: 1,
+          corner: 2,
+          yellow_card: 2,
+          red_card: 0,
+          overTime_score: 0,
+          penalty_score: 0,
+        },
+        tournament: {
+          id: "ym2xwfiotyu669p",
+          priority: 9,
+          name: "La Liga",
+          slug: "la-liga",
+          category: {
+            name: "Spain",
+            slug: "spain",
+            id: "3hc6ro1wp0p3bmq",
+            flag: "spain",
+          },
+          group_num: 0,
+        },
+        homeTeam: {
+          id: "s4luxy1o0nbrihp",
+          name: "UD Las Palmas",
+          slug: "ud-las-palmas",
+          shortName: "Las Palmas",
+        },
+        awayScore: {
+          current: 1,
+          display: 1,
+          period1: 0,
+          period2: 1,
+          corner: 6,
+          yellow_card: 1,
+          red_card: 0,
+          overTime_score: 0,
+          penalty_score: 0,
+        },
+        awayTeam: {
+          id: "bm0nxito4lns3t6",
+          name: "Deportivo Alavés",
+          slug: "deportivo-alavs",
+          shortName: "Alavés",
+        },
+        status: {
+          code: 100,
+          description: "ended",
+          type: "finished",
+        },
+        winnerCode: 3,
+        startTimestamp: 1716732900,
+        stage_id: "mx7a61lemw6se70",
+      },
+      {
+        id: "4m9applq5v8yr6b",
+        homeScore: {
+          current: 0,
+          display: 0,
+          period1: 0,
+          period2: 0,
+          corner: 4,
+          yellow_card: 2,
+          red_card: 0,
+          overTime_score: 0,
+          penalty_score: 0,
+        },
+        tournament: {
+          id: "ym2xwfiotyu669p",
+          priority: 9,
+          name: "La Liga",
+          slug: "la-liga",
+          category: {
+            name: "Spain",
+            slug: "spain",
+            id: "3hc6ro1wp0p3bmq",
+            flag: "spain",
+          },
+          group_num: 0,
+        },
+        homeTeam: {
+          id: "s4luxy1o0nbrihp",
+          name: "UD Las Palmas",
+          slug: "ud-las-palmas",
+          shortName: "Las Palmas",
+        },
+        awayScore: {
+          current: 2,
+          display: 2,
+          period1: 1,
+          period2: 1,
+          corner: 2,
+          yellow_card: 3,
+          red_card: 0,
+          overTime_score: 0,
+          penalty_score: 0,
+        },
+        awayTeam: {
+          id: "s4luxy1o353rihp",
+          name: "Girona FC",
+          slug: "girona-fc",
+          shortName: "Girona",
+        },
+        status: {
+          code: 100,
+          description: "ended",
+          type: "finished",
+        },
+        winnerCode: 2,
+        startTimestamp: 1714219200,
+        stage_id: "mx7a61lemw6se70",
+      },
+      {
+        id: "54cvbmlw373hvp4",
+        homeScore: {
+          current: 2,
+          display: 2,
+          period1: 1,
+          period2: 1,
+          corner: 4,
+          yellow_card: 2,
+          red_card: 0,
+          overTime_score: 0,
+          penalty_score: 0,
+        },
+        tournament: {
+          id: "ym2xwfiotyu669p",
+          priority: 9,
+          name: "La Liga",
+          slug: "la-liga",
+          category: {
+            name: "Spain",
+            slug: "spain",
+            id: "3hc6ro1wp0p3bmq",
+            flag: "spain",
+          },
+          group_num: 0,
+        },
+        homeTeam: {
+          id: "s4luxy1o0nbrihp",
+          name: "UD Las Palmas",
+          slug: "ud-las-palmas",
+          shortName: "Las Palmas",
+        },
+        awayScore: {
+          current: 2,
+          display: 2,
+          period1: 1,
+          period2: 1,
+          corner: 8,
+          yellow_card: 1,
+          red_card: 0,
+          overTime_score: 0,
+          penalty_score: 0,
+        },
+        awayTeam: {
+          id: "g38gsxooh9qrl7t",
+          name: "Real Betis",
+          slug: "real-betis",
+          shortName: "Real Betis",
+        },
+        status: {
+          code: 100,
+          description: "ended",
+          type: "finished",
+        },
+        winnerCode: 3,
+        startTimestamp: 1715880600,
+        stage_id: "mx7a61lemw6se70",
+      },
+      {
+        id: "o6jamrls9g17w7q",
+        homeScore: {
+          current: 0,
+          display: 0,
+          period1: 0,
+          period2: 0,
+          corner: 5,
+          yellow_card: 3,
+          red_card: 1,
+          overTime_score: 0,
+          penalty_score: 0,
+        },
+        tournament: {
+          id: "ym2xwfiotyu669p",
+          priority: 9,
+          name: "La Liga",
+          slug: "la-liga",
+          category: {
+            name: "Spain",
+            slug: "spain",
+            id: "3hc6ro1wp0p3bmq",
+            flag: "spain",
+          },
+          group_num: 0,
+        },
+        homeTeam: {
+          id: "ym2xwfiohpss006",
+          name: "Cadiz",
+          slug: "cadiz",
+          shortName: "Cadiz CF",
+        },
+        awayScore: {
+          current: 0,
+          display: 0,
+          period1: 0,
+          period2: 0,
+          corner: 3,
+          yellow_card: 1,
+          red_card: 0,
+          overTime_score: 0,
+          penalty_score: 0,
+        },
+        awayTeam: {
+          id: "s4luxy1o0nbrihp",
+          name: "UD Las Palmas",
+          slug: "ud-las-palmas",
+          shortName: "Las Palmas",
+        },
+        status: {
+          code: 100,
+          description: "ended",
+          type: "finished",
+        },
+        winnerCode: 3,
+        startTimestamp: 1716138000,
+        stage_id: "mx7a61lemw6se70",
+      },
+      {
+        id: "a8qv3rl0446xr1c",
+        homeScore: {
+          current: 1,
+          display: 1,
+          period1: 0,
+          period2: 1,
+          corner: 2,
+          yellow_card: 2,
+          red_card: 0,
+          overTime_score: 0,
+          penalty_score: 0,
+        },
+        tournament: {
+          id: "ym2xwfiotyu669p",
+          priority: 9,
+          name: "La Liga",
+          slug: "la-liga",
+          category: {
+            name: "Spain",
+            slug: "spain",
+            id: "3hc6ro1wp0p3bmq",
+            flag: "spain",
+          },
+          group_num: 0,
+        },
+        homeTeam: {
+          id: "s4luxy1o0nbrihp",
+          name: "UD Las Palmas",
+          slug: "ud-las-palmas",
+          shortName: "Las Palmas",
+        },
+        awayScore: {
+          current: 1,
+          display: 1,
+          period1: 0,
+          period2: 1,
+          corner: 6,
+          yellow_card: 1,
+          red_card: 0,
+          overTime_score: 0,
+          penalty_score: 0,
+        },
+        awayTeam: {
+          id: "bm0nxito4lns3t6",
+          name: "Deportivo Alavés",
+          slug: "deportivo-alavs",
+          shortName: "Alavés",
+        },
+        status: {
+          code: 100,
+          description: "ended",
+          type: "finished",
+        },
+        winnerCode: 3,
+        startTimestamp: 1716732900,
+        stage_id: "mx7a61lemw6se70",
+      },
+    ],
+  },
+  message: "",
+};
+
+export const Match = () => {
+  return (
+    <div className=" text-white bg-[#020c20] p-4 ">
+      <h1 className="font-bold">MATCHES</h1>
+      {data.data.events.map((item, index) => {
+        const formattedDate = new Date(
+          item.startTimestamp * 1000
+        ).toLocaleDateString("en-GB", {
+          day: "2-digit",
+          month: "2-digit",
+        });
+        return (
+          <div
+            key={index}
+            className="flex flex-row my-4 justify-between rounded-lg p-2 bg-gradient-to-r from-customblue3 via-customblue2 to-customblue1"
+          >
+            <div className="grid grid-cols-2">
+              <div className="flex flex-col justify-center items-center">
+                <p>{formattedDate}</p>
+                <p>{item.status.type}</p>
+              </div>
+              <div className="flex flex-col">
+                <div className="flex flex-row pb-2">
+                  <img
+                    src="https://img.uniscore.com/football/team/$%7Bdecode(teamId)%7D/image/small"
+                    alt="Logo"
+                  />
+                  <span>{item.homeTeam.shortName}</span>
+                </div>
+                <div className="flex flex-row">
+                  <img
+                    src="https://img.uniscore.com/football/team/$%7Bdecode(teamId)%7D/image/small"
+                    alt="Logo"
+                  />
+                  <span>{item.awayTeam.shortName}</span>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-row items-center justify-center gap-2">
+              <div className="flex flex-col">
+                <div
+                  className={`flex items-center justify-center text-[12px] h-5 w-5 rounded-t-md ${
+                    item.awayScore.current === item.homeScore.current
+                      ? "bg-[#0038E0]"
+                      : item.awayScore.current > item.homeScore.current
+                      ? "bg-gradient-to-r from-customblue3 via-customblue2 to-customblue1"
+                      : "bg-[#2187E5]"
+                  }`}
+                >
+                  {item.homeScore.current}
+                </div>
+                <div
+                  className={`flex items-center justify-center text-[12px] h-5 w-5 text-center rounded-b-md  ${
+                    item.awayScore.current === item.homeScore.current
+                      ? "bg-[#0038E0]"
+                      : item.awayScore.current > item.homeScore.current
+                      ? "bg-[#2187E5]"
+                      : "bg-gradient-to-r from-customblue3 via-customblue2 to-customblue1"
+                  }`}
+                >
+                  {item.awayScore.current}
+                </div>
+              </div>
+              <img src={Bed} alt="" className="w-5 h-5 object-cover" />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
--- demo/src/pages/home.jsx
+++ demo/src/pages/home.jsx
@@ -1,14 +1,16 @@
 import { Container } from "@/components/home/container";
 import { Header } from "@/components/home/header";
 import { Info } from "@/components/home/info";
+import { Match } from "@/components/home/match";
 
 export const Home = () => {
   return (
-    <div className="bg-black h-auto">
+    <div className="bg-black h-auto pb-12">
       <Header />
       <div className="mx-8">
         <Info />
         <Container />
+        <Match />
       </div>
     </div>
   );
